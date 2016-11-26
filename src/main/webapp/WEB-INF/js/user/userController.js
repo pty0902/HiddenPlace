793f5470da9ea74b500b5e17bd538a751281a411
pty0902 <pty0902@hanmail.net>
2016-11-26 17:52:33 +0900

    [add] br/pty
    1

---
+++ src/main/webapp/WEB-INF/js/user/userController.js
@@ -0,0 +1,36 @@
+$('head').append('<script src=\'/userDao.js\'><\/script>');
+
+// user controller 객체
+function UserController() {
+
+	var dao = new UserDao();
+
+	// 회원가입 controller 메서드
+	this.requestUserInsert = function(newUser) {
+
+		var isSuccess = dao.userInsertDao(newUser);
+
+		return isSuccess;
+
+	};
+	
+	// 닉네임 중복확인 controller 메서드
+	this.requestNickNameCheck = function(nickName) {
+		
+		var isSuccess = dao.nickNameCheckDao(nickName);
+		
+		return isSucces;
+	
+	}
+	
+	// 로그인 controller 메서드
+	this.requestLogin = function(id, pw) {
+		
+		var isSuccess = dao.userLoginDao(id, pw);
+		
+		return isSuccess;
+		
+	}
+	
+
+}
