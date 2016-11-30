506f25511f0140f483454d340078ef7b54659a4c
comstory11 <comstory11@naver.com>
2016-11-30 20:09:03 +0900

    [add]bookmark

--- src/main/java/com/phoenix/hiddenplace/controller/ControllerTest.java
+++ src/main/java/com/phoenix/hiddenplace/controller/ControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ControllerTest {
 
+
    @RequestMapping(value="/homepage/homeView")
    public String test1() {
       System.out.println("html 파일 호출");
@@ -15,6 +16,18 @@ public class ControllerTest {
    @RequestMapping(value="/homepage/homeJspTest")
    public void test2(){
       System.out.println("jsp 파일 호출");
+   }
+   
+   @RequestMapping(value="/bookmark/userBookMarkView")
+   public String test3() {
+      System.out.println("html 파일 호출");
+      return "/views/bookmark/userBookMarkView";
+   }
+   selectedDeleteMHP
+   @RequestMapping(value="/bookmark/userBookMarkView")
+   public String test3() {
+      System.out.println("html 파일 호출");
+      return "/views/bookmark/userBookMarkView";
    }
 
 }
