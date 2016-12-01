529334c6f00fc612db367646ba53bd5ea78df644
pty0902 <pty0902@hanmail.net>
2016-12-01 12:25:57 +0900

    [add]

--- src/main/java/com/phoenix/hiddenplace/controller/ControllerTest.java
+++ src/main/java/com/phoenix/hiddenplace/controller/ControllerTest.java
@@ -6,12 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ControllerTest {
 
-<<<<<<< HEAD
    @RequestMapping(value="/user/userInsertView")
-=======
-
-   @RequestMapping(value="/homepage/homeView")
->>>>>>> refs/remotes/dongjin1204/master
    public String test1() {
       System.out.println("html 파일 호출");
       return "/views/user/userInsertView";
