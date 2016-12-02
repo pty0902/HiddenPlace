3e78f254a4abbd7f51fcb7fb998456185b68838e
pty0902 <pty0902@hanmail.net>
2016-12-02 19:49:27 +0900

    [add]

--- src/main/java/com/phoenix/hiddenplace/controller/UserController.java
+++ src/main/java/com/phoenix/hiddenplace/controller/UserController.java
@@ -1,12 +1,8 @@
 package com.phoenix.hiddenplace.controller;
 
-import javax.inject.Inject;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.phoenix.hiddenplace.service.UserService;
 
 @Controller
 @RequestMapping(value = "/user")
