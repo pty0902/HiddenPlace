d46eded1e435f6386368f043ec85b6a9226c8b19
dongjin1204 <92dongjin@gmail.com>
2016-12-03 09:31:31 +0900

    [merge]Sun

--- src/main/java/com/phoenix/hiddenplace/HomeController.java
+++ src/main/java/com/phoenix/hiddenplace/HomeController.java
@@ -21,6 +21,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 
+		System.out.println("12.3 9:31am");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
