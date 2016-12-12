57dcfe967130c5a680daad46c84b5ac2bc62a669
LeeDoUk <adws92@naver.com>
2016-12-12 14:25:17 +0900

    [add] bookMark

---
+++ src/main/java/com/phoenix/hiddenplace/service/BookMarkService.java
@@ -0,0 +1,11 @@
+package com.phoenix.hiddenplace.service;
+
+
+
+public interface BookMarkService {
+
+
+	// 북마크 선택삭제
+	public void bookMarkDelete(String nums) throws Exception;
+
+}
--- src/main/webapp/resources/js/bookmark/bookMarkController.js
+++ src/main/webapp/resources/js/bookmark/bookMarkController.js
@@ -1,1 +1,20 @@
-1
+$('head').append('<script src=\'../../../resources/js/bookmark/bookMarkDao.js\'><\/script>');
+
+
+//bookmark controller 객체
+function BookMarkController() {
+
+	var dao = new BookMarkDao();	
+
+	
+//	선택 글삭제 controller 메서드
+	this.requestSelectedDeleteMHP = function(delete_nums) {
+		alert(delete_nums);
+		dao.selectedDeleteMHP(delete_nums);
+
+
+	};
+	
+
+	
+}
