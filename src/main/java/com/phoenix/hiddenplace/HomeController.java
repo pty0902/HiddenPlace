c57df63f6facf0da6c3c74b577f0b108cec3fc7f
dongjin1204 <92dongjin@gmail.com>
2016-12-15 10:25:16 +0900

    [update]master

--- src/main/java/com/phoenix/hiddenplace/HomeController.java
+++ src/main/java/com/phoenix/hiddenplace/HomeController.java
@@ -21,7 +21,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 
-		System.out.println("12.14 4:32pm");
+		System.out.println("12.15 10:20pm");
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
